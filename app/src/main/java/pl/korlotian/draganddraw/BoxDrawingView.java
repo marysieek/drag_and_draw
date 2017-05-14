ed1a341e805b6652e1cfcbff1d2a8645cdd82b31
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-05-14 14:56:58 +0200

    Create own paint

--- app/src/main/java/pl/korlotian/draganddraw/BoxDrawingView.java
+++ app/src/main/java/pl/korlotian/draganddraw/BoxDrawingView.java
@@ -1,15 +1,25 @@
 package pl.korlotian.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -17,6 +27,12 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
     }
 
     @Override
@@ -27,15 +43,23 @@ public class BoxDrawingView extends View {
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
 
@@ -43,5 +67,19 @@ public class BoxDrawingView extends View {
             ", y=" +current.y);
 
         return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 }
